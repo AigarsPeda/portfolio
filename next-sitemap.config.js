0b83f3e4932df40ddfc23571ab45a22a622267ca
Aigars <aigarspeda@gmail.com>
2023-05-11 08:57:56 +0300

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+const siteUrl = "https://www.aigarspeda.com";
+
+module.exports = {
+  siteUrl,
+  exclude: ["/404"],
+  generateRobotsTxt: true,
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,9 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Aigars</title>
+        <title>AP</title>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Aigars Peda portfolio" />
         <link rel="icon" href="/favicon.ico" />
