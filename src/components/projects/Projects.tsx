3e39872b98927a5050f631eb8e1d58d171332c67
Aigars <aigarspeda@gmail.com>
2023-09-01 06:43:19 +0300

    updated index page

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,15 +1,33 @@
+import { motion } from "framer-motion";
 import { type FC } from "react";
 import GridLayout from "~/components/GridLayout/GridLayout";
 import ProjectCard from "~/components/ProjectCard/ProjectCard";
-import { PROJECTS } from "~/hardcoded";
+import { PROJECTS, PROJECT_APPEARED_DELAY } from "~/hardcoded";
 import getUniqueId from "~/utils/getUniqueId";
 
 const Projects: FC = () => {
   return (
     <GridLayout isGap minWith="250px">
-      {PROJECTS.map((project) => (
-        <ProjectCard key={getUniqueId()} project={project} />
-      ))}
+      {PROJECTS.map((project, i) => {
+        const delay = PROJECT_APPEARED_DELAY + 0.5 * i;
+
+        return (
+          <motion.div
+            key={getUniqueId()}
+            initial={{ opacity: 0, y: 200 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay,
+              velocity: 1,
+              bounce: 0.2,
+              duration: 1.2,
+              type: "spring",
+            }}
+          >
+            <ProjectCard project={project} />
+          </motion.div>
+        );
+      })}
     </GridLayout>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { motion, type Variants } from "framer-motion";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Mailto from "~/components/Mailto/Mailto";
@@ -8,6 +9,31 @@ import useWindowSize from "~/hooks/useWindowSize";
 
 const Home: NextPage = () => {
   const { windowSize } = useWindowSize();
+
+  const container: Variants = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0.1,
+        // staggerChildren: 0.5,
+      },
+    },
+  };
+
+  const item: Variants = {
+    hidden: { opacity: 0, scale: 0.6 },
+    show: {
+      scale: 1,
+      opacity: 1,
+      transition: {
+        velocity: 0.8,
+        bounce: 0.2,
+        duration: 1.7,
+        type: "spring",
+      },
+    },
+  };
 
   return (
     <>
@@ -20,38 +46,56 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="mx-auto max-w-7xl items-center px-8 pt-8">
-        <div className="grid w-full md:h-96 md:grid-cols-10">
+      <main className="mx-auto max-w-7xl items-center px-8 pt-8 md:pt-16">
+        <motion.div
+          animate="show"
+          initial="hidden"
+          variants={container}
+          className="grid w-full md:h-72 md:grid-cols-10"
+        >
           <div className="col-span-5 mb-5 flex h-full w-full items-center justify-center">
-            <div className="w-full">
+            <motion.div variants={item} className="w-full">
               <h1 className="mb-5 text-4xl md:mb-4 md:text-5xl">
                 Hey! My name is Aigars and I am front-end developer.
               </h1>
 
               <Mailto body="From: aigarspeda.com" email="aigarspeda@gmail.com">
-                <p className="mb-8 tracking-wider text-primary-accent md:mb-0">
+                <p className="mb-4 tracking-wider text-primary-accent md:mb-0">
                   aigarspeda@gmail.com
                 </p>
               </Mailto>
-            </div>
+            </motion.div>
           </div>
 
-          <div className="col-span-5 h-full w-full">
+          <motion.div
+            variants={item}
+            className="col-span-5 h-full w-full max-w-lg md:ml-10"
+          >
             {windowSize.width <= 415 ? (
               <ProgrammerMob />
             ) : (
               <ProgrammerDesktop />
             )}
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
 
-        <div className="mt-[15%]">
+        <motion.div
+          initial={{ opacity: 0, y: 200 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            velocity: 1,
+            bounce: 0.1,
+            duration: 1.5,
+            type: "spring",
+          }}
+          className="mt-[15%]"
+        >
           <h2 className="mb-5 text-3xl text-primary-light/50 md:mb-8 md:pt-0 md:text-4xl">
             Projects
           </h2>
 
           <Projects />
-        </div>
+        </motion.div>
       </main>
     </>
   );
