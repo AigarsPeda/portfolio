b455ee3c167b9771d4bde00573b7bbd5352dc1b2
Aigars <aigarspeda@gmail.com>
2023-09-02 06:53:43 +0300

    updated index page

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -3,16 +3,21 @@ import { type FC } from "react";
 import GridLayout from "~/components/GridLayout/GridLayout";
 import ProjectCard from "~/components/ProjectCard/ProjectCard";
 import { PROJECTS, PROJECT_APPEARED_DELAY } from "~/hardcoded";
+import useWindowSize from "~/hooks/useWindowSize";
 import getUniqueId from "~/utils/getUniqueId";
 
 const Projects: FC = () => {
+  const { windowSize } = useWindowSize();
+
   return (
     <GridLayout isGap minWith="250px">
       {PROJECTS.map((project, i) => {
-        const delay = PROJECT_APPEARED_DELAY + 0.5 * i;
+        const delay =
+          windowSize.width <= 500 ? i / 2 : PROJECT_APPEARED_DELAY + 0.5 * i;
 
         return (
           <motion.div
+            className="mb-8"
             key={getUniqueId()}
             initial={{ opacity: 0, y: 200 }}
             whileInView={{ opacity: 1, y: 0 }}
