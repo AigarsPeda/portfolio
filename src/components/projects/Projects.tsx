e4a917468392fe86a28115cef3bead73d1a726b1
Aigars <aigarspeda@gmail.com>
2023-09-25 17:44:42 +0300

    fixed warnings

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,57 +1,56 @@
-import { motion, type Variants } from "framer-motion";
+import { motion } from "framer-motion";
 import { type FC } from "react";
 import ProjectCard from "~/components/ProjectCard/ProjectCard";
 import { PROJECTS } from "~/hardcoded";
-import useWindowSize from "~/hooks/useWindowSize";
 import getUniqueId from "~/utils/getUniqueId";
 
 const Projects: FC = () => {
-  const { windowSize } = useWindowSize();
+  // const { windowSize } = useWindowSize();
 
-  const isOdd = (num: number) => num % 2 === 1;
-  const isMobile = windowSize.width <= 500;
+  // const isOdd = (num: number) => num % 2 === 1;
+  // const isMobile = windowSize.width <= 500;
 
-  const animationVariants = (num: number): Variants => {
-    if (isMobile) {
-      return {
-        offscreen: {
-          x: isOdd(num) ? 200 : -200,
-          opacity: 0,
-        },
-        onscreen: {
-          y: 0,
-          x: 0,
-          opacity: 1,
-          // rotate: isOdd(num) ? 1 : -1,
-          transition: {
-            bounce: 0.2,
-            duration: 1.2,
-            delay: num / 2 - 0.2,
-            type: "spring",
-          },
-        },
-      };
-    }
+  // const animationVariants = (num: number): Variants => {
+  //   if (isMobile) {
+  //     return {
+  //       offscreen: {
+  //         x: isOdd(num) ? 200 : -200,
+  //         opacity: 0,
+  //       },
+  //       onscreen: {
+  //         y: 0,
+  //         x: 0,
+  //         opacity: 1,
+  //         // rotate: isOdd(num) ? 1 : -1,
+  //         transition: {
+  //           bounce: 0.2,
+  //           duration: 1.2,
+  //           delay: num / 2 - 0.2,
+  //           type: "spring",
+  //         },
+  //       },
+  //     };
+  //   }
 
-    return {
-      offscreen: {
-        y: 200,
-        opacity: 0,
-      },
-      onscreen: {
-        y: 0,
-        x: 0,
-        opacity: 1,
-        // rotate: isOdd(num) ? 1 : -1,
-        transition: {
-          bounce: 0.2,
-          duration: 1.2,
-          delay: num / 2,
-          type: "spring",
-        },
-      },
-    };
-  };
+  //   return {
+  //     offscreen: {
+  //       y: 200,
+  //       opacity: 0,
+  //     },
+  //     onscreen: {
+  //       y: 0,
+  //       x: 0,
+  //       opacity: 1,
+  //       // rotate: isOdd(num) ? 1 : -1,
+  //       transition: {
+  //         bounce: 0.2,
+  //         duration: 1.2,
+  //         delay: num / 2,
+  //         type: "spring",
+  //       },
+  //     },
+  //   };
+  // };
 
   return (
     <div className="flex flex-col flex-wrap gap-10 md:flex-row">
