9b62a5b4b2c467f21e868362e1239ad50a2afc5e
Aigars <aigarspeda@gmail.com>
2023-11-03 22:56:35 +0200

    build fix

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,46 +1,27 @@
-import { motion, type Variants } from "framer-motion";
+import { motion } from "framer-motion";
 import { type FC } from "react";
 import ProjectCard from "~/components/ProjectCard/ProjectCard";
 import { PROJECTS } from "~/hardcoded";
 import useWindowSize from "~/hooks/useWindowSize";
-import getUniqueId from "~/utils/getUniqueId";
 
 const Projects: FC = () => {
   const { windowSize } = useWindowSize();
   const isMobile = windowSize.width <= 500;
 
-  // const container: Variants = {
-  //   hidden: { opacity: 0 },
-  //   show: {
-  //     opacity: 1,
-  //     transition: {
-  //       delayChildren: 0.1,
-  //       // staggerChildren: 0.5,
-  //     },
-  //   },
-  // };
-
-  // const item: Variants = {
-  //   hidden: { opacity: 0, scale: 0.8 },
-  //   show: {
-  //     scale: 1,
-  //     opacity: 1,
-  //     transition: {
-  //       velocity: 0.8,
-  //       bounce: 0.2,
-  //       duration: 1.8,
-  //       type: "spring",
-  //     },
-  //   },
-  // };
-
   return (
     <div className="flex flex-col flex-wrap gap-10 md:flex-row">
       {PROJECTS.map((project, i) => {
+        // if (isMobile) {
+        //   return (
+        //     <div key={i}>
+        //       <ProjectCard project={project} />
+        //     </div>
+        //   );
+        // }
+
         return (
           <motion.div
-            key={getUniqueId()}
-            // variants={item}
+            key={i}
             initial={"hidden"}
             whileInView="visible"
             viewport={{ once: true }}
@@ -59,9 +40,8 @@ const Projects: FC = () => {
                 },
               },
               hidden: {
-                // x: isMobile ? 0 : i % 2 === 0 ? -100 : 100,
-                y: isMobile ? 0 : 100,
-                opacity: isMobile ? 1 : 0,
+                y: 150,
+                opacity: 0,
               },
             }}
           >
